e94deeadcd2c853d67198ce5b3367f668da6170e
kuang <867748221@163.com>
2018-05-14 13:06:24 +0800

    添加helloworld测试代码

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by AbelKung on 2018/5/14.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.print("hello world");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaStd
 Java学习中的练习文档
+
+2018-05-14 添加helloworld测试代码
